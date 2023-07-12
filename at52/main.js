d049453c0ec8fa0eb0b5aaa33afdf54af6d01cf0
MahnoorHaider <125372033+mahnoorhyder-bit@users.noreply.github.com>
2023-07-12 23:18:39 +0500

    Add files via upload

---
+++ at52/main.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+//// 43-48
+// var n = [0, 1, 2, 3];
+
+// for (var i = 0; i < n.length; i++) {
+//     document.getElementById(`d${i + 1}`).innerHTML = n[i];
+// }
+
+// function myDelete(rNumber, dNumber) {
+//     document.getElementById(rNumber).innerHTML = "";
+//     console.log(dNumber);
+//     if (dNumber === "d1") {
+//         for (var i = 0; i < n.length; i++) {
+            
+//         }
+//     }
+
+    
+//     var d = document.getElementById(dNumber).innerHTML;
+//     var index = n.indexOf(parseInt(d));
+//     n.splice(index, 1);
+// }
+
+// function counter1() {
+//     document.getElementById("count").innerHTML++; 
+// }
+// function counter2() {
+//     document.getElementById("count").innerHTML--; 
+// }
+
+
